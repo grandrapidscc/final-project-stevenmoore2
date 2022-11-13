31819c99739d2f86d47cba19dd513dc31a4cf3cc
stevenmoore2 <stevenmoore2@email.grcc.edu>
2022-11-13 14:51:55 -0500

    Layer Layer 1: Continue or Quit (11/13/22 Push)
    - Created the system that asks if the player would like to play again. If they type 1, the program loops. If they type 2, the program stops. If anything else is typed, they will have to retype their answer.
    - Added a class that contains a starting outline for what is needed to finish the project.

---
+++ CIS129FinalProject/Game.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+public class game
+{
+    // Layer 1: Continue or Quit
+    //    -  Remember FIXME
+    //
+    // Layer 2: The Map
+    // a. Decide the locations of Every Square (Cannot Overlap, Player doesn't have access to Info)
+    //    - 1 Wizert (Start)
+    //    - 1 Exit
+    //    - Enemies
+    //          - 5 Goblins
+    //          - 5 Orcs
+    //          - 3 Banshees
+    //    - Potions
+    //          - 3 Health Potions
+    //          - 3 Magicka Potions
+    //    
+    //    
+    // b. Navigation
+    //    - North, South, East, West
+    //    - 5x5 Grid (Player has no map, walls only outside of the array.)
+    //    - Encounter Order:
+    //          1. Enemy Encounter (If Available)
+    //          2. Powerup Found (If Available)
+    //          3. Description of Room
+    //          4. Choose a Direction
+    //          
+    // Layer 3: Fights
+    // a. Create Characters and Attacks
+    //   - Wizert (The Player)
+    //      - 100 HP
+    //      - 200 MP
+    //          - Fireball (3 MP)
+    //          - Heal (5 MP)
+    //          - Flee (0 MP, 1st: 50%, 2nd: 75%, 3rd: Guaranteed)
+    //  
+    // 
+    //   - Goblin
+    //      - 3 HP
+    //          - Body Slam (2 HP towards Wizert)
+    //   - Orc
+    //      - 5 HP
+    //          - Cleave (3 HP towards Wizert)
+    //   - Banshee
+    //      - 8 HP
+    //          - Screech (5 HP towards Wizert)
+    //
+    // b. Battle System Outline
+    // 
+    //  Entering the room, you encounter a Banshee!
+    //  
+    //  HP: 100   MP: 200
+    //  What will Wizert do?
+    //
+    //  Press...
+    //
+    //  1. Cast Fireball (3 MP)
+    //  2. Heal yourself (5 MP)
+    //  3. Flee from the fight
+    //
+    //  Wizert Casts Fireball!
+    //
+    //  The Banshee takes 5 damage.
+    //
+    //  The Banshee Screeches at Wizert!
+    //
+    //  Wizert takes 5 damage.
+    //
+    //  HP: 95   MP: 197
+    //  What will Wizert do?
+    //
+    //  Press...
+    //
+    //  1. Cast Fireball (3 MP)
+    //  2. Heal yourself (5 MP)
+    //  3. Flee from the fight
+    //
+    //  Wizert Casts Fireball!
+    //
+    //  The Banshee takes 5 damage.
+    //  The Banshee falls!
+    //  
+    //  
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+
+
+
+
+
+
+}
--- CIS129FinalProject/Program.cs
+++ CIS129FinalProject/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// This is where the program starts.
+
+bool gamer = true;
+
+while (gamer == true)
+{
+    //FIXME: Swap out this line when rest of game is finished.
+    Console.WriteLine("Game has been played and finished.");
+    
+    //The player is prompted if they would like to play again and must choose one of two options.
+    Console.WriteLine("Would you like to play again?\nPress... \n1. Yes\n2. No");
+
+    //This loop takes the player's first input and immediatly takes an answer.
+    while (gamer == true) 
+    {
+        //Player's input.
+        var c = Console.ReadKey(false).Key;
+        
+        Console.WriteLine("");
+        // If the player types the number 2 (Top Row or Num Pad), then the program will stop.
+        if (c == ConsoleKey.NumPad2 || c == ConsoleKey.D2)
+        {
+            gamer = false;
+        }
+        // If the player types the number 1 (Top Row or Num Pad). The current loop will end and the program will start the game once again.
+        else if (c == ConsoleKey.NumPad1 || c == ConsoleKey.D1)
+        {
+            break;
+        }
+        // If the player doesn't type either 1 or 2, then the player will be told their input was invalid and will have to try again.
+        else
+        {
+            Console.WriteLine("\nInvalid Option.\nPress... \n1. Yes\n2. No");
+        }
+    }
+}
+//The program thanks the player for their time before terminating.
+Console.WriteLine("Thanks for Playing!");
